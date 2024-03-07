5d6581c81dcc7e503a65fecceaa016756c71cc99
Ashish <rbzashu1@gmail.com>
2024-03-07 12:20:03 +0530

    added tailwind css

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Body = () => {
-  return <div>Body</div>;
+  return (
+    <>
+      <div className="text-center py-5">
+        <span className="text-5xl text-amber-800 font-bold">
+          FAST REACT PIZZA CO.
+        </span>
+      </div>
+      <div className="text-center my-5">
+        <span className="border-dotted border-4 border-black border-x-0 p-3 text-3xl font-semibold text-violet-800">
+          OUR MENU
+        </span>
+      </div>
+    </>
+  );
 };
 
 export default Body;
