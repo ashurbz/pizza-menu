525cf0c081395db1902ab9c60083e4287ac67ebe
Ashish <rbzashu1@gmail.com>
2024-03-11 12:35:26 +0530

    added pizzas cards and apply disabled on sold out

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,10 @@
 import React from "react";
+import PizzaCard from "./PizzaCard";
+import { pizzaData } from "../data";
 
 const Body = () => {
   return (
-    <div className="bg-slate-400 w-screen h-screen">
+    <div className="bg-slate-400 w-full h-full">
       <div className="text-center py-5">
         <span className="text-5xl text-amber-800 font-bold">
           FAST REACT PIZZA CO.
@@ -18,6 +20,9 @@ const Body = () => {
           Authentic Italion cuisinie & createive dishes to <br /> choose from,
           All from over stove oven, all organic, <br /> all delicious
         </span>
+      </div>
+      <div>
+        <PizzaCard data={pizzaData} />
       </div>
     </div>
   );
