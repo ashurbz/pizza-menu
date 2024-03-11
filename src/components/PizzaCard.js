a07a32c1b0c99e57411d32893106fc6fdb6ef47e
Ashish <rbzashu1@gmail.com>
2024-03-11 12:35:49 +0530

    created Footer

--- src/components/PizzaCard.js
+++ src/components/PizzaCard.js
@@ -1,12 +1,28 @@
 import React from "react";
-import { pizzaData } from "../data";
 
-const PizzaCard = () => {
+const PizzaCard = ({ data }) => {
+  console.log(data);
+
   return (
-    <div>
-      <div>
-        <img />
-      </div>
+    <div className="flex">
+      {data.map((item) => {
+        return (
+          <div
+            className={` ${
+              item.soldOut ? "bg-red-500" : ""
+            } border border-stone-800 w-1/3 p-5 m-2 justify-between`}
+          >
+            <div className="w-1/2">
+              <img className="rounded-2xl" src={item.photoName} alt="foca" />
+            </div>
+            <div className="p-3">
+              <div className="font-bold font-serif">{item.name}</div>
+              <div>{item.ingredients}</div>
+              <div>{item.soldOut ? "Sold Out" : item.price}</div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
